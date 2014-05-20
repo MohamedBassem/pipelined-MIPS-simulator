8f4c046c8581f927dc57e12eb9c44386c9a673ed
MohamedBassem <medoox240@gmail.com>
2014-05-20 13:20:59 +0300

    Fixing the datamemory

--- src/eg/edu/guc/mimps/components/DataMemory.java
+++ src/eg/edu/guc/mimps/components/DataMemory.java
@@ -1,10 +1,32 @@
 package eg.edu.guc.mimps.components;
 
+import java.util.HashMap;
+
 import eg.edu.guc.mimps.registers.ExecuteMemoryRegisters;
 import eg.edu.guc.mimps.registers.MemoryWritebackRegisters;
 
 public class DataMemory implements Executable {
 
+	ExecuteMemoryRegisters executeMemoryRegisters;
+	MemoryWritebackRegisters memoryWritebackRegisters;
+	
+	HashMap<Integer,Integer> memory;
+	
+	// MemoryWritebackRegisters
+	private int ALUResult;
+	private int memoryWord;
+	private int writeBackRegister;
+	
+	// Write Back Signals
+	private boolean regWrite;
+	private boolean memToReg;
+
+	public DataMemory(ExecuteMemoryRegisters executeMemoryRegister,
+			MemoryWritebackRegisters memoryWritebackRegisters) {
+		this.executeMemoryRegisters = executeMemoryRegisters;
+		this.memoryWritebackRegisters = memoryWritebackRegisters;
+		
+	}
 
 	@Override
 	public void execute() {
