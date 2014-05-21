60453333f89c4fe7212808808471aa3e3c1cf8fd
Ahmed Akram <a.akram93@gmail.com>
2014-05-21 10:34:52 +0200

    array of registers

--- src/eg/edu/guc/mimps/registers/Registers.java
+++ src/eg/edu/guc/mimps/registers/Registers.java
@@ -1,5 +1,18 @@
 package eg.edu.guc.mimps.registers;
 
 public class Registers {
-
+	private int[] registers;
+	
+	public Registers() {
+		registers = new int[32];
+	}
+	
+	public int getReg(int reg) {
+		return registers[reg];
+	}
+	
+	public void setReg(int reg, int data) {
+		registers[reg] = data;
+	}
+	
 }
