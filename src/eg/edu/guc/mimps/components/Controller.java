e44b4c48a57717aba76e1695c4dbf790233a1b0e
sak93 <sarakhodeir@gmail.com>
2014-05-23 20:41:16 +0300

    controller

--- src/eg/edu/guc/mimps/components/Controller.java
+++ src/eg/edu/guc/mimps/components/Controller.java
@@ -1,46 +1,56 @@
 package eg.edu.guc.mimps.components;
 
 import eg.edu.guc.mimps.assembler.Instruction;
-import eg.edu.guc.mimps.assembler.InstructionFormat;
 import eg.edu.guc.mimps.registers.InstructionDecodeExecuteRegisters;
 import eg.edu.guc.mimps.registers.InstructionFetchDecodeRegisters;
 
 public class Controller implements Executable {
-	String rformatControls = "1000010";
-	String iformatControls = "0100010";
-	String jformatControls = ""; 
-	String loadControls = "0101011";
-	String storeControls = "X10010X";
-	String branchControls = "X01000X";
+	String rformatControls = "10000102";
+	String iformatControls = "01000102";
+	String jformatControls = "XXXXXXX3";
+	String loadControls = "01010110";
+	String storeControls = "X10010X0";
+	String branchControls = "X01000X1";
 	String control;
-	
-	public  InstructionFetchDecodeRegisters instructionFetchDecodeRegisters; 
+
+	public InstructionFetchDecodeRegisters instructionFetchDecodeRegisters;
 	public InstructionDecodeExecuteRegisters instructionDecodeExecuteRegisters;
+
 	public Controller(
 			InstructionFetchDecodeRegisters instructionFetchDecodeRegisters,
 			InstructionDecodeExecuteRegisters instructionDecodeExecuteRegisters) {
-		this.instructionFetchDecodeRegisters= instructionFetchDecodeRegisters;
+		this.instructionFetchDecodeRegisters = instructionFetchDecodeRegisters;
 		this.instructionDecodeExecuteRegisters = instructionDecodeExecuteRegisters;
-		
+
 	}
 
 	@Override
 	public void execute() {
-		int opCode=0; // will get this value from previous component
-		String instructionType=toInstructionType(opCode); 
-		 this.control = getControlString(instructionType);
+		Instruction instruction = instructionFetchDecodeRegisters
+				.getInstruction();
+		String instructionType = this.toInstructionType(instruction.getOpcode());
+		this.control = getControlString(instructionType);
 
 	}
 
 	@Override
 	public void write() {
-		instructionDecodeExecuteRegisters.setRegDest(toBoolean(control.charAt(0)));
-		instructionDecodeExecuteRegisters.setAluSrc(toBoolean(control.charAt(1)));
-		instructionDecodeExecuteRegisters.setBranch(toBoolean(control.charAt(2)));
-		instructionDecodeExecuteRegisters.setMemRead(toBoolean(control.charAt(3)));
-		instructionDecodeExecuteRegisters.setMemWrite(toBoolean(control.charAt(4)));
-		instructionDecodeExecuteRegisters.setRegWrite(toBoolean(control.charAt(5)));
-		instructionDecodeExecuteRegisters.setMemToReg(toBoolean(control.charAt(6)));
+		instructionDecodeExecuteRegisters.setRegDest(toBoolean(control
+				.charAt(0)));
+		instructionDecodeExecuteRegisters
+				.setAluSrc(toBoolean(control.charAt(1)));
+		instructionDecodeExecuteRegisters
+				.setBranch(toBoolean(control.charAt(2)));
+		instructionDecodeExecuteRegisters.setMemRead(toBoolean(control
+				.charAt(3)));
+		instructionDecodeExecuteRegisters.setMemWrite(toBoolean(control
+				.charAt(4)));
+		instructionDecodeExecuteRegisters.setRegWrite(toBoolean(control
+				.charAt(5)));
+		instructionDecodeExecuteRegisters.setMemToReg(toBoolean(control
+				.charAt(6)));
+		instructionDecodeExecuteRegisters.setAluOpt(Integer.parseInt(""
+				+ control.charAt(7)));
 	}
 
 	public boolean toBoolean(char value) {
@@ -52,7 +62,7 @@ public class Controller implements Executable {
 
 	public String getControlString(String instructionType) {
 		switch (instructionType) {
-		
+
 		case "rType":
 			return rformatControls;
 
@@ -67,24 +77,43 @@ public class Controller implements Executable {
 
 		case "store":
 			return storeControls;
-		
+
 		case "jType":
-			return jformatControls; 
+			return jformatControls;
 
 		}
 		return instructionType;
 	}
-	public String toInstructionType(int opCode){
-	String type="";
-	switch(opCode){
-	//will change case depending on opCodes 
-	case 1 :type= "rType" ; break;
-	case 2: type= "iType"; break; 
-	case 3: type= "jType"; break; 
-	case 4: type= "branch"; break; 
-	case 5: type= "store";break;
-	case 6: type= "load"; break; 
-	}
-	return type;
+
+	public String toInstructionType(int opCode) {
+		String type = "";
+		switch (opCode) {
+		// will change case depending on opCodes
+		case 0:
+			type = "rType";
+			break;
+		case 1:
+		case 4:
+		case 5:
+			type = "iType";
+			break;
+		case 3:
+			type = "store";
+			break;
+		case 2:
+			type = "load";
+			break;
+		case 6:
+		case 7:
+			type = "branch";
+			break;
+		case 9:
+		case 10:
+		case 11:
+			type = "jType";
+			break;
+
+		}
+		return type;
 	}
 }
