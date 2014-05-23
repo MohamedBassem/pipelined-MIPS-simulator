527fe509fe597787df8596d94bf3d35a7da98a4c
MohamedBassem <medoox240@gmail.com>
2014-05-23 23:23:07 +0300

    Editing ALU

--- src/eg/edu/guc/mimps/components/ALU.java
+++ src/eg/edu/guc/mimps/components/ALU.java
@@ -1,6 +1,5 @@
 package eg.edu.guc.mimps.components;
 
-import eg.edu.guc.mimps.assembler.Instruction;
 import eg.edu.guc.mimps.registers.ExecuteMemoryRegisters;
 import eg.edu.guc.mimps.registers.InstructionDecodeExecuteRegisters;
 import eg.edu.guc.mimps.utils.BinaryManiplator;
@@ -12,6 +11,17 @@ public class ALU implements Executable {
 	ExecuteMemoryRegisters executeMemoryRegister;
 	ExecuteMemoryRegisters newExecuteMemoryRegister;
 	
+	private final static int AND  = 0b0000;
+	private final static int OR   =  0b0001;
+	private final static int ADD  = 0b0010;
+	private final static int SUB  = 0b0110;
+	private final static int SLT  = 0b0111;
+	
+	private final static int SLL  = 0b0011;
+	private final static int SRL  = 0b0100;
+	private final static int NOR  = 0b0101;
+	private final static int SLTU = 0b1000;
+	
 	public ALU(
 			InstructionDecodeExecuteRegisters instructionDecodeExecuteRegisters,
 			ExecuteMemoryRegisters executeMemoryRegisters) {
@@ -21,42 +31,81 @@ public class ALU implements Executable {
 
 
 	public void execute() {
-		newExecuteMemoryRegister = executeMemoryRegister.clone();
-		int ALUResultALU = 0;
-		boolean zeroALU = false;
-		int register1Value = decodeExecuteRegister.getRegister1Value();
-		int register2Value = decodeExecuteRegister.getRegister2Value();
-		int signExtendedOffset = decodeExecuteRegister.getSignExtendedOffset();
-		//will be fixed after fixing opcodes
-		int opCode = decodeExecuteRegister.getAluOpt();
-		if(opCode == 0) {
-			switch (opCode){
-			case Constants.ADD_FUNC: ALUResultALU = add(register1Value, register2Value);break;
-			case Constants.SUB_FUNC: ALUResultALU = sub(register1Value, register2Value);break;
-			case Constants.SLL_FUNC: ALUResultALU = shiftLeftLogical(register1Value, register2Value);break;
-			case Constants.SRL_FUNC: ALUResultALU = shiftRightLogical(register1Value, register2Value);break;
-			case Constants.AND_FUNC: ALUResultALU = and(register1Value, register2Value);break;
-			case Constants.OR_FUNC: ALUResultALU = or(register1Value, register2Value);break;
-			case Constants.NOR_FUNC: ALUResultALU = nor(register1Value, register2Value);break;
-			case Constants.SLT_FUNC: ALUResultALU = setLessThan(register1Value, register2Value);break;
-			case Constants.SLTU_FUNC: ALUResultALU = setLessThanUnsigned(register1Value, register2Value);break;
-			default:break;
-			}
-		} else {
-			switch (opCode) {
-			case Constants.ADDI_OPCODE:
-			case Constants.SW_OPCODE:
-			case Constants.LW_OPCODE: ALUResultALU = add(register1Value, signExtendedOffset);break;
-			case Constants.ANDI_OPCODE: ALUResultALU = and(register1Value, signExtendedOffset);break;
-			case Constants.ORI_OPCODE: ALUResultALU = or(register1Value, signExtendedOffset);break;
-			case Constants.BEQ_OPCODE: zeroALU = branchIfEqual(register1Value, register2Value);break;
-			case Constants.BNE_OPCODE: zeroALU = branchIfNotEqual(register1Value, register2Value);break;
-			default:break;
-			}
+		int aluOpt = decodeExecuteRegister.getAluOpt();
+		int functionCode = BinaryManiplator.getPartialValue(decodeExecuteRegister.getSignExtendedOffset(), 0, 5);
+		
+		int aluOperation = 0;
+		
+		switch(aluOpt){
+			// Lw or Sw
+			case 0: aluOperation = ADD; break;
+			// Beq
+			case 1: aluOperation = SUB; break;
+			// R-Instruction
+			case 2: 
+				switch(functionCode){
+					case Constants.ADD_FUNC  : aluOperation = ADD;break;
+					case Constants.OR_FUNC   : aluOperation = OR;break;
+					case Constants.AND_FUNC  : aluOperation = AND;break;
+					case Constants.NOR_FUNC  : aluOperation = NOR;break;
+					case Constants.SUB_FUNC  : aluOperation = SUB;break;
+					case Constants.SLL_FUNC  : aluOperation = SLL;break;
+					case Constants.SRL_FUNC  : aluOperation = SRL;break;
+					case Constants.SLT_FUNC  : aluOperation = SLT;break;
+					case Constants.SLTU_FUNC : aluOperation = SLTU;break;
+				}
+				break;
+			// Jump
+			case 3: //TODO
+				break;
+			// BNE
+			case 4: aluOperation = SUB; break;
+			// ADDI
+			case 5: aluOperation = ADD; break;
+			// ANDI
+			case 6: aluOperation = AND; break;
+			// ORI
+			case 7: aluOperation = OR; break;
 		}
 		
-		newExecuteMemoryRegister.setZero(zeroALU);
-		newExecuteMemoryRegister.setALUResult(ALUResultALU);
+		int first = decodeExecuteRegister.getRegister1Value();
+		int second;
+		if(decodeExecuteRegister.isAluSrc()){
+			second = decodeExecuteRegister.getSignExtendedOffset();
+		}else{
+			second = decodeExecuteRegister.getRegister2Value();
+		}
+		int shamt = decodeExecuteRegister.getShamt();
+		int aluResult = 0;
+		switch(aluOperation){
+			case ADD : aluResult = add(first, second);break;
+			case SUB : aluResult = sub(first, second);break;
+			case AND : aluResult = and(first, second);break;
+			case OR  : aluResult = or(first, second);break;
+			case NOR : aluResult = nor(first, second);break;
+			case SLL : aluResult = shiftLeftLogical(first, shamt);break;
+			case SRL : aluResult = shiftRightLogical(first, shamt);break;
+			case SLT : aluResult = setLessThan(first, second);break;
+			case SLTU: aluResult = setLessThanUnsigned(first, second);break;
+		}
+		
+		boolean zero = false;
+		if(aluOpt == 1){
+			zero = (aluResult == 0);
+		}else if(aluOpt == 4){
+			zero = (aluResult != 0);
+		}
+		newExecuteMemoryRegister = executeMemoryRegister.clone();
+		
+		newExecuteMemoryRegister.setZero(zero);
+		newExecuteMemoryRegister.setBranchAddress(decodeExecuteRegister.getIncrementedPc() + decodeExecuteRegister.getSignExtendedOffset()*4);
+		newExecuteMemoryRegister.setRegisterValueToMemory(decodeExecuteRegister.getRegister2Value());
+		if(decodeExecuteRegister.isRegDest()){
+			newExecuteMemoryRegister.setWriteBackRegister(decodeExecuteRegister.getRd());
+		}else{
+			newExecuteMemoryRegister.setWriteBackRegister(decodeExecuteRegister.getRt());
+		}
+		newExecuteMemoryRegister.setALUResult(aluResult);
 		newExecuteMemoryRegister.setMemRead(decodeExecuteRegister.isMemRead());
 		newExecuteMemoryRegister.setMemWrite(decodeExecuteRegister.isMemWrite());
 		newExecuteMemoryRegister.setBranch(decodeExecuteRegister.isBranch());
@@ -109,24 +158,12 @@ public class ALU implements Executable {
 		return 0;
 	}
 	
-	private int shiftLeftLogical(int first, int second) {
-		return first << second;
+	private int shiftLeftLogical(int first, int shamt) {
+		return first << shamt;
 	}
 	
-	private int shiftRightLogical(int first, int second) {
-		return first >>> second;
-	}
-	
-	private boolean branchIfEqual(int first, int second){
-		if (first == second)
-			return true;
-		return false;
-	}
-	
-	private boolean branchIfNotEqual(int first, int second) {
-		if (first != second)
-			return true;
-		return false;
+	private int shiftRightLogical(int first, int shamt) {
+		return first >>> shamt;
 	}
 
 }
