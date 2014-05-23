39ad7c56b244fa2e7955eecd2b2a04169cbfdc86
sak93 <sarakhodeir@gmail.com>
2014-05-23 21:46:56 +0300

    controller code

--- src/eg/edu/guc/mimps/components/Controller.java
+++ src/eg/edu/guc/mimps/components/Controller.java
@@ -1,26 +1,94 @@
 package eg.edu.guc.mimps.components;
 
+import eg.edu.guc.mimps.assembler.Instruction;
 import eg.edu.guc.mimps.registers.InstructionDecodeExecuteRegisters;
 import eg.edu.guc.mimps.registers.InstructionFetchDecodeRegisters;
 
 public class Controller implements Executable {
+	String rformatControls = "10000102";
+	String iformatControls = "01000102";
+	String jformatControls = "XXXXXXX3";
+	String loadControls = "01010110";
+	String storeControls = "X10010X0";
+	String branchControls = "X01000X1";
+	String control;
+
+	public InstructionFetchDecodeRegisters instructionFetchDecodeRegisters;
+	public InstructionDecodeExecuteRegisters instructionDecodeExecuteRegisters;
 
 	public Controller(
 			InstructionFetchDecodeRegisters instructionFetchDecodeRegisters,
 			InstructionDecodeExecuteRegisters instructionDecodeExecuteRegisters) {
-		// TODO Auto-generated constructor stub
+		this.instructionFetchDecodeRegisters = instructionFetchDecodeRegisters;
+		this.instructionDecodeExecuteRegisters = instructionDecodeExecuteRegisters;
+
 	}
 
 	@Override
 	public void execute() {
-		// TODO Auto-generated method stub
-		
+		int instructionNumber = instructionFetchDecodeRegisters
+				.getInstruction();
+		Instruction instruction = new Instruction(instructionNumber);
+		this.toInstructionType(instruction.getOpcode());
+
 	}
 
 	@Override
 	public void write() {
-		// TODO Auto-generated method stub
-		
+		instructionDecodeExecuteRegisters.setRegDest(toBoolean(control
+				.charAt(0)));
+		instructionDecodeExecuteRegisters
+				.setAluSrc(toBoolean(control.charAt(1)));
+		instructionDecodeExecuteRegisters
+				.setBranch(toBoolean(control.charAt(2)));
+		instructionDecodeExecuteRegisters.setMemRead(toBoolean(control
+				.charAt(3)));
+		instructionDecodeExecuteRegisters.setMemWrite(toBoolean(control
+				.charAt(4)));
+		instructionDecodeExecuteRegisters.setRegWrite(toBoolean(control
+				.charAt(5)));
+		instructionDecodeExecuteRegisters.setMemToReg(toBoolean(control
+				.charAt(6)));
+		instructionDecodeExecuteRegisters.setAluOpt(Integer.parseInt(""
+				+ control.charAt(7)));
 	}
-	
+
+	public boolean toBoolean(char value) {
+		if (value == '1') {
+			return true;
+		}
+		return false;
+	}
+
+
+
+	public void toInstructionType(int opCode) {
+		
+		switch (opCode) {
+		// will change case depending on opCodes
+		case 0x00:
+			control =rformatControls; 
+			break;
+		case 0x08:
+		case 0x0C:
+		case 0x0D:
+			control=iformatControls; 
+			break;
+		case 0x2B:
+			control = storeControls; 
+			break;
+		case 0x23:
+			control = loadControls; 
+			break;
+		case 0x04:
+		case 0x05:
+			control = branchControls;
+			break;
+		case 0x02:
+		case 0x03:
+			control =jformatControls; 
+			break;
+
+		}
+	}
 }
