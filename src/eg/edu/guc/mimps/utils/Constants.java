db1b8163a5b43b5ffefd660884e4d39688b7e098
MohamedBassem <medoox240@gmail.com>
2014-05-23 19:38:35 +0300

    Real Opcodes

--- src/eg/edu/guc/mimps/utils/Constants.java
+++ src/eg/edu/guc/mimps/utils/Constants.java
@@ -29,38 +29,40 @@ public class Constants {
 	/*
 	 * Opcodes
 	 */
-	public static final int ADD_OPCODE 	= 0;
-	public static final int SUB_OPCODE 	= 0;
-	public static final int SLL_OPCODE 	= 0;
-	public static final int SRL_OPCODE 	= 0;
-	public static final int AND_OPCODE 	= 0;
-	public static final int OR_OPCODE 	= 0;
-	public static final int NOR_OPCODE 	= 0;
-	public static final int SLT_OPCODE 	= 0;
-	public static final int SLTU_OPCODE = 0;
-	public static final int ADDI_OPCODE = 1;
-	public static final int LW_OPCODE 	= 2;
-	public static final int SW_OPCODE 	= 3;
-	public static final int ANDI_OPCODE = 4;
-	public static final int ORI_OPCODE 	= 5;
-	public static final int BEQ_OPCODE 	= 6;
-	public static final int BNE_OPCODE 	= 7;
-	public static final int J_OPCODE 	= 8;
-	public static final int JAL_OPCODE 	= 9;
-	public static final int JR_OPCODE 	= 10;
+	public static final int ADD_OPCODE 	= 0x00;
+	public static final int SUB_OPCODE 	= 0x00;
+	public static final int SLL_OPCODE 	= 0x00;
+	public static final int SRL_OPCODE 	= 0x00;
+	public static final int AND_OPCODE 	= 0x00;
+	public static final int OR_OPCODE 	= 0x00;
+	public static final int NOR_OPCODE 	= 0x00;
+	public static final int SLT_OPCODE 	= 0x00;
+	public static final int SLTU_OPCODE = 0x00;
+	public static final int JR_OPCODE 	= 0x00;
+	public static final int ADDI_OPCODE = 0x08;
+	public static final int LW_OPCODE 	= 0x23;
+	public static final int SW_OPCODE 	= 0x2B;
+	public static final int ANDI_OPCODE = 0x0C;
+	public static final int ORI_OPCODE 	= 0x0D;
+	public static final int BEQ_OPCODE 	= 0x04;
+	public static final int BNE_OPCODE 	= 0x05;
+	public static final int J_OPCODE 	= 0x02;
+	public static final int JAL_OPCODE 	= 0x03;
+	
 		
 	/*
 	 * Func
 	 */
-	public static final int ADD_FUNC 	= 0;
-	public static final int SUB_FUNC 	= 1;
-	public static final int SLL_FUNC 	= 2;
-	public static final int SRL_FUNC 	= 3;
-	public static final int AND_FUNC 	= 4;
-	public static final int OR_FUNC 	= 5;
-	public static final int NOR_FUNC 	= 6;
-	public static final int SLT_FUNC 	= 7;
-	public static final int SLTU_FUNC 	= 8;
+	public static final int ADD_FUNC 	= 0x20;
+	public static final int SUB_FUNC 	= 0x22;
+	public static final int SLL_FUNC 	= 0x00;
+	public static final int SRL_FUNC 	= 0x02;
+	public static final int AND_FUNC 	= 0x24;
+	public static final int OR_FUNC 	= 0x25;
+	public static final int NOR_FUNC 	= 0x27;
+	public static final int SLT_FUNC 	= 0x2A;
+	public static final int SLTU_FUNC 	= 0x2B;
+	public static final int JR_FUNC 	= 0x08;
 	
 	
 }
