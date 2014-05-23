6671b26afb59356281e0b6ca9e3e78c13e7e705e
Alaa Maher <almgohar@gmail.com>
2014-05-23 16:28:47 +0200

    Adding update

--- src/eg/edu/guc/mimps/gui/GUI.java
+++ src/eg/edu/guc/mimps/gui/GUI.java
@@ -3,10 +3,13 @@ package eg.edu.guc.mimps.gui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.util.Vector;
+import java.beans.PropertyChangeListener;
 
+import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
@@ -16,14 +19,27 @@ import javax.swing.JSplitPane;
 import javax.swing.JTable;
 import javax.swing.JTextPane;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.text.Document;
-
-import com.sun.org.apache.xerces.internal.impl.dv.ValidatedInfo;
+import eg.edu.guc.mimps.registers.Memory;
+import eg.edu.guc.mimps.registers.Registers;
+import eg.edu.guc.mimps.simulator.Simulator;
 
 public class GUI {
+	Simulator simulator;
+	Memory memory;
+	Registers registers;
+	JFrame mainFrame;
+	JTable registersTable;
+	JTable memoryTable;
+	JTextPane editor;
+
+	public GUI(Simulator simulator, Memory memory, Registers registers) {
+		this.simulator = simulator;
+		this.memory = memory;
+		this.registers = registers;		
+	}
 
 	public void startFrame() {
-		JFrame mainFrame = new JFrame();
+		mainFrame = new JFrame();
 		JMenuBar menu = getMenuBar();
 		mainFrame.setMinimumSize(new Dimension((int) Toolkit
 				.getDefaultToolkit().getScreenSize().getWidth()-70,
@@ -44,38 +60,8 @@ public class GUI {
 		mainFrame.setVisible(true);
 	}
 
-	private JMenuBar getMenuBar() {
-		JMenuBar mb = new JMenuBar();
-		JMenuItem run = new JMenuItem(new ImageIcon("run.png"));
-		run.setMaximumSize(new Dimension(70, 100));
-		run.setDisabledIcon(new ImageIcon("run_disabled.png"));
-		JMenuItem stop = new JMenuItem(new ImageIcon("stop.png"));
-		stop.setDisabledIcon(new ImageIcon("stop_disabled.png"));
-		stop.setMaximumSize(new Dimension(70, 100));
-		JMenuItem runStep = new JMenuItem(new ImageIcon("run_step.png"));
-		runStep.setDisabledIcon(new ImageIcon("run_step_disabled.png"));
-		runStep.setMaximumSize(new Dimension(70, 100));
-		JMenuItem save = new JMenuItem(new ImageIcon("save.png"));
-		save.setMaximumSize(new Dimension(70, 100));
-		JMenuItem open = new JMenuItem(new ImageIcon("open.png"));
-		open.setMaximumSize(new Dimension(70, 100));
-		JMenuItem assemble = new JMenuItem(new ImageIcon("assemble.png"));
-		assemble.setDisabledIcon(new ImageIcon("assemble_disabled.png"));
-		assemble.setMaximumSize(new Dimension(70, 100));
-		run.setEnabled(false);
-		runStep.setEnabled(false);
-		stop.setEnabled(false);
-		mb.add(open);
-		mb.add(save);
-		mb.add(assemble);
-		mb.add(run);
-		mb.add(runStep);
-		mb.add(stop);
-		return mb;
-	}
-
 	private JScrollPane getEditor() {
-		JTextPane editor = new JTextPane();
+		editor = new JTextPane();
 		editor.addKeyListener
 		(
 		new KeyListener()
@@ -107,7 +93,67 @@ public class GUI {
 		return scroll;
 	}
 	
-	public JScrollPane getRegistersTable() {
+	private JMenuBar getMenuBar() {
+		JMenuBar mb = new JMenuBar();
+		final JMenuItem run = new JMenuItem(new ImageIcon("run.png"));
+		run.setMaximumSize(new Dimension(70, 100));
+		run.setDisabledIcon(new ImageIcon("run_disabled.png"));
+		final JMenuItem runStep = new JMenuItem(new ImageIcon("run_step.png"));
+		runStep.setDisabledIcon(new ImageIcon("run_step_disabled.png"));
+		runStep.setMaximumSize(new Dimension(70, 100));
+		JMenuItem save = new JMenuItem(new ImageIcon("save.png"));
+		save.setMaximumSize(new Dimension(70, 100));
+		JMenuItem open = new JMenuItem(new ImageIcon("open.png"));
+		open.setMaximumSize(new Dimension(70, 100));
+		JMenuItem assemble = new JMenuItem(new ImageIcon("assemble.png"));
+		assemble.setDisabledIcon(new ImageIcon("assemble_disabled.png"));
+		assemble.setMaximumSize(new Dimension(70, 100));
+		run.setEnabled(false);
+		runStep.setEnabled(false);
+		
+		assemble.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(simulator.assemble(0,editor.getText())) {
+					run.setEnabled(true);
+					runStep.setEnabled(true);
+				} else {
+					
+				}
+				
+			}
+		});
+		
+		run.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				simulator.run(0, editor.getText());
+				run.setEnabled(false);
+				runStep.setEnabled(false);
+			}
+		});
+		
+		runStep.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(!simulator.step()) {
+					runStep.setEnabled(false);
+					run.setEnabled(true);
+				}
+			}
+		});
+		mb.add(open);
+		mb.add(save);
+		mb.add(assemble);
+		mb.add(run);
+		mb.add(runStep);
+		return mb;
+	}
+	
+	private JScrollPane getRegistersTable() {
 		String[] names = { "Register", "Number", "Value" };
 		String[] registers = { "$zero", "$at", "$v0", "$v1", "$a0", "$a1",
 				"$a2", "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6",
@@ -115,7 +161,7 @@ public class GUI {
 				"$t8", "$t9", "$k0", "$k1", "$gp", "$sp", "$fp", "$ra" };
 
 		DefaultTableModel model = new DefaultTableModel(names, 35);
-		JTable registersTable = new JTable();
+		registersTable = new JTable();
 		registersTable.setModel(model);
 		for (int i = 0; i < registers.length; i++) {
 			registersTable.setValueAt(registers[i], i, 0);
@@ -123,7 +169,8 @@ public class GUI {
 		}
 		registersTable.setValueAt("pc", 32, 0);
 		registersTable.setValueAt("hi", 33, 0);
-		registersTable.setValueAt("lo", 34, 0);
+		registersTable.setValueAt("lo", 34, 0);	
+		registersTable.setEnabled(false);
 		JScrollPane scroll = new JScrollPane(registersTable);
 		scroll.setMinimumSize(new Dimension(400, Toolkit.getDefaultToolkit()
 				.getScreenSize().height));
@@ -132,22 +179,35 @@ public class GUI {
 
 	private JScrollPane getMemoryTable() {
 		DefaultTableModel model = new DefaultTableModel(10, 10);
-		JTable memoryTable = new JTable(model);
+		memoryTable = new JTable(model);
 		memoryTable.getColumn(memoryTable.getColumnName(0)).setHeaderValue(
 				"Address");
 		for (int i = 1; i < 10; i++) {
 			memoryTable.getColumn(memoryTable.getColumnName(i)).setHeaderValue(
 					"Value" + i);
 		}
-
+		memoryTable.setEnabled(false);
 		JScrollPane scroll = new JScrollPane(memoryTable);
 		scroll.setMinimumSize(new Dimension(700, 200));
 		return scroll;
 	}
 
-	public static void main(String[] args) {
-		GUI gui = new GUI();
+	public void update() {
+		for (int i = 0; i < 32; i++) {
+			registersTable.setValueAt(registers.getReg(i), i, 2);
+		}
+		registersTable.setValueAt(simulator.getPc(), 32, 2);
+	}
+	
+	public static void main(String[]args) {
+		Registers registers = new Registers();
+		Memory memory = new Memory();
+		Simulator simulator = new Simulator();
+		
+		GUI gui = new GUI(simulator, memory, registers);
 		gui.startFrame();
+		gui.update();
+
 	}
 
 }
