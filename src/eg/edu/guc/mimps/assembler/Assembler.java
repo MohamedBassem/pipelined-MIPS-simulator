38d640430d4ef89d33b751da5b230601f33c6249
farghalico <mohamed@farghal.com>
2014-05-24 02:02:13 +0200

    change test output

--- src/eg/edu/guc/mimps/assembler/Assembler.java
+++ src/eg/edu/guc/mimps/assembler/Assembler.java
@@ -76,7 +76,7 @@ public class Assembler {
 	
 	public void print() {
 		for (Integer key : instructions.keySet()) {
-			System.out.println(key + " : " + instructions.get(key).getConstant());
+			System.out.println(key + " : " + instructions.get(key).toString());
 		}
 	}
 	
